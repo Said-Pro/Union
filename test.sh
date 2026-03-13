2a23311759f62fd2ae531951f6ac9523490471a9
Said-Pro <haddawi0924@gmail.com>
2026-03-13 03:13:01 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,5 @@
 wget -O /tmp/UnionStream.tar.gz https://github.com/Said-Pro/Union/raw/refs/heads/main/UnionStream.tar.gz
-
-
 cd /tmp/
 tar -xzf UnionStream.tar.gz -C /usr/lib/enigma2/python/Plugins/Extensions
-
 rm -f /tmp/UnionStream.tar.gz
-
 killall -9 enigma2
-
-systemctl restart enigma2
